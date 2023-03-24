6e815b4714034e1a065b7481741db394107b771b
phomolomankge <mankgephomolo@gmail.com>
2023-03-24 19:51:56 +0200

    testUpdate

--- roadTest/js/question.js
+++ roadTest/js/question.js
@@ -6,25 +6,106 @@ const quiz = [
         options:['No left turns','Overtaking compalsary','No overtaking','No parking'],
         answer:2
     },
+    
+    {q: "What does a solid red traffic light mean?",
+    options: ["Stop", "Go", "Slow down", "Yield"],
+    answer: 0
+},  
+
+    {q: "What should you do when you approach a roundabout?",
+    options: ["Slow down and proceed with caution", "Speed up and proceed through quickly", "Stop and wait for the roundabout to clear", "Honk your horn to alert other drivers"],
+    answer: 0
+},
+  {  q: "What is the maximum speed limit in a residential area?",
+    options: ["25 mph", "30 mph", "35 mph", "40 mph"],
+    answer: 0
+}, 
+   {q: "When can you legally pass another vehicle on the right?",
+    options: ["When the vehicle is turning left", "When the vehicle is in the left lane", "When there is a dedicated right-hand lane", "When there is heavy traffic"],
+    answer: 2
+},
+  {  q: "What does a solid yellow line on your side of the center line mean?",
+    options: ["No passing allowed", "Passing allowed with caution", "Passing allowed with signaling", "Passing allowed at any time"],
+    answer: 0
+}, 
     {
-        q:'A continuous yellow line down the middle of the road means what?',
-        options:['No left turns','Overtaking compalsary','No overtaking','No parking'],
-        answer:2
+        q: "What is the correct hand signal for a left turn?",
+    options: ["Left arm straight out", "Left arm pointed up", "Left arm pointed down", "Right arm pointed up"],
+    answer: 0
+    },
+
+    {
+        q: "What is the minimum following distance you should maintain behind the vehicle in front of you?",
+        options: ["1 car length", "2 car lengths", "3 car lengths", "4 car lengths"],
+        answer: 2
+    },
+    
+   
+    
+    {
+        q: "What should you do if your vehicle starts to hydroplane?",
+    options: ["Brake firmly and come to a stop", "Accelerate and steer into the skid", "Ease off the accelerator and steer straight ahead", "Turn the steering wheel sharply to the left or right"],
+    answer: 2
+    },
+    
+    {
+        q: "What is the legal blood alcohol content (BAC) limit for drivers over the age of 21?",
+    options: ["0.08%", "0.10%", "0.12%", "0.14%"],
+    answer: 0
+    },
+    
+   { q: "What is the purpose of an anti-lock braking system (ABS)?",
+    options: ["To reduce skidding when braking", "To improve fuel efficiency", "To increase engine power", "To improve traction on wet roads"],
+    answer: 0
+},
+{
+q: "What should you do if your car starts to skid?",
+options: ["Apply the brakes immediately", "Steer in the opposite direction of the skid", "Accelerate to gain control", "Keep the steering wheel straight"],
+answer: 1
+},
+{
+    q: "What is the purpose of a speed limit sign?",
+    options: ["To indicate the recommended speed for the road", "To indicate the minimum speed for the road", "To indicate the maximum speed for the road", "To indicate the average speed for the road"],
+    answer: 2
     },
     {
-        q:'A continuous yellow line down the middle of the road means what?',
-        options:['No left turns','Overtaking compalsary','No overtaking','No parking'],
-        answer:2
+        q: "What is the purpose of a catalytic converter in a car?",
+        options: ["To reduce emissions", "To increase engine power", "To improve fuel efficiency", "To improve traction on wet roads"],
+        answer: 0
+        },
+        
+        {
+        q: "What should you do if you see a pedestrian crossing the road?",
+        options: ["Speed up to avoid them", "Honk your horn to let them know you're coming", "Slow down and stop if necessary", "Swerve around them"],
+        answer: 2
+        },
+        {
+q: "What should you do if you encounter a school bus with flashing red lights?",
+options: ["Pass the bus quickly", "Slow down and proceed with caution", "Ignore the lights and continue driving", "Honk your horn to alert the driver"],
+answer: 1
+},
+{
+    q: "What is the purpose of a blind spot when driving a car?",
+    options: ["To help you see clearly in all directions", "To help you avoid obstacles on the road", "To help you merge or change lanes safely", "To help you park in tight spaces"],
+    answer: 2
     },
     {
-        q:'A continuous yellow line down the middle of the road means what?',
-        options:['No left turns','Overtaking compalsary','No overtaking','No parking'],
-        answer:2
-    },
-    {
-        q:'A continuous yellow line down the middle of the road means what?',
-        options:['No left turns','Overtaking compalsary','No overtaking','No parking'],
-        answer:2,
-        img: '/img/wrong/.png'
-    }
+        q: "What is the purpose of a rearview mirror in a car?",
+        options: ["To see what's behind the car", "To see what's in front of the car", "To see what's on the sides of the car", "To see what's inside the car"],
+        answer: 0
+        },
+        {
+            q: "What should you do if you are driving and a tire blows out?",
+            options: ["Brake suddenly to stop the car", "Accelerate to get off the road quickly", "Gently apply the brakes and steer to the side of the road", "Continue driving until you reach a service station"],
+            answer: 2
+            },
+            {
+                q: "What should you do if your car starts to skid on a wet road?",
+                options: ["Brake hard to stop the car", "Accelerate to gain traction", "Steer in the opposite direction of the skid", "Steer in the direction of the skid"],
+                answer: 3
+                }
+    
+    
+    
+    
 ]
